41f0711bf39805595030380e61bef98661150e0b
vtang <78068910+VTyiu@users.noreply.github.com>
2022-10-03 20:45:37 -0700

    Fixed node index.js not working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,19 @@
 // ./src/index.js
 
 // importing the dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const {startDatabase} = require('./database/mongo');
-const {insertAd, getAds} = require('./database/ads');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { startDatabase } = require("./database/mongo");
+const { insertAd, getAds } = require("./database/ads");
 
 // defining the Express app
 const app = express();
 
 // defining an array to work as the database (temporary solution)
-const ads = [
-    {title: 'Hello, world (again)!'}
-];
+const ads = [{ title: "Hello, world (again)!" }];
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -27,17 +25,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // adding morgan to log HTTP requests
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // defining an endpoint to return all ads
-app.get('/', async (req, res) => {
-    res.send(await getAds());
+app.get("/", async (req, res) => {
+  res.send(await getAds());
 });
 
-app.post('/test', async (req, res) => {
-    res.send({ message: 'test works' });
-  });
-  
+app.post("/test", async (req, res) => {
+  res.send({ message: "test works" });
+});
+
 // startDatabase().then(async () => {
 //     await insertAd({title: 'Hello, now from the in-memory database!'});
 
@@ -48,7 +46,7 @@ app.post('/test', async (req, res) => {
 
 // });
 
-    // starting the server
-    app.listen(3001, () => {
-        console.log('listening on port 3001');
-    });
+// starting the server
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
