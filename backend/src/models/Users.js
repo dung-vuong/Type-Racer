aaf4c250f2e50cc2bc97d6230a41a0634bc81f5b
vtang <78068910+VTyiu@users.noreply.github.com>
2022-10-05 14:20:43 -0700

    added model schemas

--- backend/src/models/Users.js
+++ backend/src/models/Users.js
@@ -5,4 +5,29 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  statsID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Stats",
+    required: false,
+  },
+  dictionaryID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Dictionary",
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = mongoose.model("users", UserSchema);
