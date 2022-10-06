6c2a4a9b99f05a35fe867849a75dfb5dc44d1e2d
Max Panec <maxwellpanec@gmail.com>
2022-10-05 19:20:21 -0700

    Add prototype of typing game

--- frontend/react_app/src/Components/TypingPage/HalfMinuteGame.jsx
+++ frontend/react_app/src/Components/TypingPage/HalfMinuteGame.jsx
@@ -1,25 +1,109 @@
 import React from 'react';
-import { useMemo } from 'react';
-import '../../Styles/TypingPage/HalfMinuteGame.css';
+import { useMemo, useRef, useEffect, useState } from 'react';
+import './HalfMinuteGame.css';
+import TypingGameLetter from './TypingGameLetter.jsx';
+import TypingGameWord from './TypingGameWord.jsx';
+
 
 const HalfMinuteGame = (props)  => {
-
-    console.log('moo');
-
-    const displayWords = useMemo(() => {
-        let list = [];
-        props.words.forEach((data) => {
-            list.push(<div className='word'>{data} </div>);
-        })
-        return list;
+    const [letterStatuses, setLetterStatuses] = useState(new Array(1000).fill("default"));
+    const [wordList, setWordList] = useState([])
+    const [letterPosition, setLetterPosition] = useState(0)
+    
+    let displayLetters = useMemo(() => {
+        let displayLetters = [];
+        let tempList = [];
+        let pos = 0;
+        props.words.forEach((word) => {
+            word = word + " ";
+            let letterList = [];
+            for (let i = 0; i < word.length; i++) {
+                displayLetters.push(word[i])
+                letterList.push(<TypingGameLetter letter={word[i]} key={pos} status={letterStatuses[pos]}/>);
+                pos++;
+            }
+            tempList.push(<TypingGameWord letters={letterList}/>)
+        });
+        setWordList(tempList)
+        return displayLetters
     },
     [props.words]
     );
 
+    const [textInput, setTextInput] = useState("");
+
+    const hiddenInputFocus = useRef(null);
+
+    const setHiddenInputFocus = () => {
+        hiddenInputFocus.current.focus();
+    }
+
+    const processTextInput = (e) => {
+        if(e.key == "Shift"){
+            return
+        }
+        if(e.key == displayLetters[letterPosition]){
+            let temp = letterStatuses
+            temp[letterPosition] = "correct"
+            setLetterStatuses(temp)
+            let tempList = [];
+            let pos = 0;
+            props.words.forEach((word) => {
+                word = word + " ";
+                let letterList = [];
+                for (let i = 0; i < word.length; i++) {
+                    letterList.push(<TypingGameLetter letter={word[i]} key={pos} status={letterStatuses[pos]}/>);
+                    pos++;
+                }
+                tempList.push(<TypingGameWord letters={letterList}/>)
+            });
+            setWordList(tempList)
+            setLetterPosition(letterPosition + 1)
+        }
+        else if(e.key == "Backspace"){
+            if(letterPosition < 1)
+                return
+            let temp = letterStatuses
+            temp[letterPosition-1] = "default"
+            setLetterStatuses(temp)
+            let tempList = [];
+            let pos = 0;
+            props.words.forEach((word) => {
+                word = word + " ";
+                let letterList = [];
+                for (let i = 0; i < word.length; i++) {
+                    letterList.push(<TypingGameLetter letter={word[i]} key={pos} status={letterStatuses[pos]}/>);
+                    pos++;
+                }
+                tempList.push(<TypingGameWord letters={letterList}/>)
+            });
+            setWordList(tempList)
+            setLetterPosition(letterPosition - 1)
+        }
+        else {
+            let temp = letterStatuses
+            temp[letterPosition] = "incorrect"
+            setLetterStatuses(temp)
+            let tempList = [];
+            let pos = 0;
+            props.words.forEach((word) => {
+                word = word + " ";
+                let letterList = [];
+                for (let i = 0; i < word.length; i++) {
+                    letterList.push(<TypingGameLetter letter={word[i]} key={pos} status={letterStatuses[pos]}/>);
+                    pos++;
+                }
+                tempList.push(<TypingGameWord letters={letterList}/>)
+            });
+            setWordList(tempList)
+            setLetterPosition(letterPosition + 1)
+        }
+    }
+
     return (
         <div className='game'>
-        <input className='gameInput'></input>
-        <div>{displayWords}</div>
+            <input className='gameInput' onKeyDown={processTextInput} ref={hiddenInputFocus} value="" readOnly></input>
+            <div onMouseUp={setHiddenInputFocus}>{wordList}</div>
         </div>
     )
 }
--- frontend/react_app/src/Components/TypingPage/TypingPage.jsx
+++ frontend/react_app/src/Components/TypingPage/TypingPage.jsx
@@ -5,6 +5,18 @@ import HalfMinuteGame from './HalfMinuteGame';
 const words = ['I', 'like', 'to', 'eat', 'pie'];
 
 const TypingPage = ()  => {
+
+    /*const tempAPI = () => {
+        let b = 0
+        const data = fetch('http://localhost:3001/test', {
+            method: 'POST',
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data["message"]));
+        return data
+    }
+
+    const words = tempAPI()*/
     return (
         <HalfMinuteGame words={words}/>
     )
